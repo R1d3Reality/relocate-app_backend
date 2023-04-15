814a49fa961b5363ef0228ed40a4527d73fa1386
Pasha <r1d3rreality@gmail.com>
2023-04-15 19:45:04 +0200

    add all local files

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  // ...
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        blog: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
+});
